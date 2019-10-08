0dd2c54797731343e89632d862d1f8509727a45b
rootgog <dev.georgebishop@gmail.com>
2019-10-08 15:37:28 +0100

    Hexadecimal converter
    String to Hex and reversed implemented, added a home link and a link to the page from home.

---
+++ assets/js/hex.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+
+    let textInput = document.querySelector("textarea[name=text]");
+    let hexInput = document.querySelector("textarea[name=hex]");
+
+    textInput.addEventListener("keyup", textToHEX);
+    hexInput.addEventListener("keyup", HEXToText);
+
+
+    function textToHEX() {
+        let str = textInput.value;
+        let parts = [];
+        for (let n = 0, l = str.length; n < l; n++) {
+            let hex = Number(str.charCodeAt(n)).toString(16);
+            parts.push(hex);
+        }
+        hexInput.value = parts.join('');
+    }
+
+    function HEXToText() {
+        let hex = hexInput.value.toString();
+        let str = '';
+        for (let n = 0; n < hex.length; n += 2) {
+            str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+        }
+        textInput.value = str;
+    }
+
+    textToHEX();
+
+}
